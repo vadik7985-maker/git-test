342814d0b6ec609ff85618dd1512958094544570
Vadim Matveev <vadik7985@gmail.com>
2025-10-03 12:24:39 +0300

    Решение задачи 4: собственный итератор

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-# 1.Создать список квадратов чисел от 1 до 10 с использованием list comprehension
+# 1.Простое преобразование
 print('список квадратов чисел от 1 до 10:', [x**2 for x in range(1, 10)])
 
-# 2.С помощью list comprehension получить список только четных чисел от 1 до 20
+# 2.Фильтрация
 print('список только четных чисел от 1 до 20:', [x for x in range(1, 20) if x % 2 == 0])
 
-# 3.Дан список слов, создать новый список, где все слова будут в верхнем регистре и длиннее 3 символов
+# 3.Работа со строками
 words = ['python', 'Java', 'c++', 'Rust', 'go']
 print([w.upper() for w in words if len(w) > 3])
+
+# 4.Собственный итератор
+class Countdown():
+    def __init__(self, max_value):
+        self.value = max_value
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.value == 0:
+            raise StopIteration
+        
+        carrent = self.value
+        self.value -= 1
+        return carrent
+    
+for i in Countdown(5):
+    print(i)
+    
